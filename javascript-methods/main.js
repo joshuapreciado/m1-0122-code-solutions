24a1ff18703c059c7a4c6834b8e33f3ec9df2f87
joshuapreciado <preciadojoshua340@yahoo.com>
2022-02-21 19:39:42 -0800

    Testjavascript-methods.

---
+++ javascript-methods/main.js
@@ -0,0 +1,10 @@
+var number1 = 96;
+var number2 = 6;
+var number3 = 9;
+var maximumValue = Math.max(number1, number2, number3);
+console.log('maximumValue:', maximumValue);
+
+var heroes = ['Superman', 'Batman', 'Spiderman', 'Deadpool',];
+var randomNumber = Math.random(3);
+var randomIndex = randomNumber * heroes.length;
+console.log('randomIndex', randomIndex);
