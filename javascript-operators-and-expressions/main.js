2948a5fa5e5c0d82f7ea3ebf4ab7de7a6568664b
joshuapreciado <preciadojoshua340@yahoo.com>
2022-02-21 19:37:57 -0800

    Completed javascript-operators-and-expressions.

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,39 @@
+var width = 4;
+var height = 7;
+var area = width * height;
+console.log('value of area:', area)
+console.log('typeof area:', typeof area);
+
+var bill = 4;
+var payment = 8;
+var change = payment - bill;
+console.log('value of change:', change)
+console.log('typeof change:', typeof change);
+
+var quizzes = 29;
+var midterm = 30;
+var final = 31;
+var grade = (quizzes + midterm + final);
+console.log('value of grade:', grade);
+console.log('typeof grade:', typeof grade);
+
+var firstName = 'Joshua';
+var lastName = 'Preciado';
+var fullName = firstName + ' ' + lastName;
+console.log('value of fullName:', fullName);
+console.log('typeof fullName:',typeof fullName)
+
+var ph = 17;
+var isAcidic = 7 < 17;
+console.log('value of isAcidic:', isAcidic);
+console.log('typeof isAcidic:', typeof isAcidic);
+
+
+var headCount = 3;
+var isSparta = headCount === 300;
+console.log('value of isSparta:', isSparta);
+console.log('typeof isSparta:', typeof isSparta);
+
+var motto = fullName + ' is the Goat';
+console.log('value of motto:', motto);
+console.log('typeof motto:', typeof motto);
