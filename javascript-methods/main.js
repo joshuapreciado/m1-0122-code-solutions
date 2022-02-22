6d182dbb7020cb7d19ca6111be4bed2dfbde19f2
joshuapreciado <preciadojoshua340@yahoo.com>
2022-02-21 21:16:11 -0800

    Not finished yet.

--- javascript-methods/main.js
+++ javascript-methods/main.js
@@ -4,7 +4,10 @@ var number3 = 9;
 var maximumValue = Math.max(number1, number2, number3);
 console.log('maximumValue:', maximumValue);
 
-var heroes = ['Superman', 'Batman', 'Spiderman', 'Deadpool',];
+var heroes = ['Superman', 'Batman', 'Spiderman', 'Deadpool'];
 var randomNumber = Math.random(3);
-var randomIndex = randomNumber * heroes.length;
-console.log('randomIndex', randomIndex);
+var randomNumber = (randomNumber * heroes.length);
+var randomIndex = Math.floor(randomNumber);
+console.log('randomIndex:', randomIndex);
+var randomHero = heroes[randomIndex];
+console.log('randomHero:', randomHero);
