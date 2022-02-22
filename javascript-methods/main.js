d0d851d1befc8adfd9dae7b75400be23b6d1b855
joshuapreciado <preciadojoshua340@yahoo.com>
2022-02-21 22:27:39 -0800

    Completed javascript-methods exercise.

--- javascript-methods/main.js
+++ javascript-methods/main.js
@@ -11,3 +11,45 @@ var randomIndex = Math.floor(randomNumber);
 console.log('randomIndex:', randomIndex);
 var randomHero = heroes[randomIndex];
 console.log('randomHero:', randomHero);
+
+var library =
+[
+    {
+    title: 'Nineteen Eighty-Four',
+    author: 'George Orwell'
+    },
+    {
+      title: 'Brave New World',
+      author: 'Aldous Huxley'
+    }
+  ];
+var js = {
+  title: 'JavaScript for Impatient Programmers',
+  author: 'Dr. Axel Rauschmayer'
+};
+var css = {
+  title: 'CSS Secrets',
+  author: 'Lea Verou'
+}
+
+var lastBook = library.pop();
+console.log('lastBook:', lastBook);
+
+var firstBook = library.shift();
+console.log('firstBook:', firstBook);
+
+library.push(js);
+
+library.unshift(css);
+
+library.splice(2);
+
+console.log('library:', library);
+
+
+var fullName = 'Joshua Preciado';
+var firstAndLastName = fullName.split(' ');
+console.log('firstAndLastName:', firstAndLastName);
+var firstName = firstAndLastName[0];
+var sayMyName = firstName.toUpperCase();
+console.log('sayMyName:', sayMyName);
