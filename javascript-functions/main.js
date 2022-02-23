0cc85186d198bad4d743afaacf2ab283748749a9
joshuapreciado <preciadojoshua340@yahoo.com>
2022-02-22 20:37:25 -0800

    Completed javascript-functions.

---
+++ javascript-functions/main.js
@@ -0,0 +1,23 @@
+function convertMinutesToSeconds(minutes) {
+  return 60 * minutes;
+}
+
+function greet(name) {
+  return "Hey, " + name;
+}
+
+function getArea(width, height) {
+  return width * height;
+}
+
+function getFirstName(person) {
+  person.firstName;
+  person.lastName;
+  return person.firstName;
+}
+
+function getLastElement(array) {
+  var lastPiece = array.length - 1;
+  var lastElement = array[lastPiece];
+  return lastElement;
+}
