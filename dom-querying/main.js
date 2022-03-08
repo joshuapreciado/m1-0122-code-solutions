f30f16ea731948db75b003db9097842b147c4853
joshuapreciado <preciadojoshua340@yahoo.com>
2022-03-07 19:19:38 -0800

    Completed dom querying.

---
+++ dom-querying/main.js
@@ -0,0 +1,18 @@
+console.log('hello, world');
+var element1 = document.querySelector('h1');
+console.log(element1);
+console.dir(element1);
+
+var element5 = document.querySelector("#explanation");
+console.log(element5);
+console.dir(element5);
+
+var element7 = document.querySelector(".hint");
+console.log(element7);
+console.dir(element7);
+
+var element10 = document.querySelectorAll('p');
+console.dir(element10);
+
+var element21 = document.querySelectorAll('.example-link');
+console.dir(element21);
