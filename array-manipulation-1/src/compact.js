3764921b851cc96c1775deedc03c5f2623533db3
joshuapreciado <preciadojoshua340@yahoo.com>
2022-03-31 18:57:03 -0700

    Completed array manip1

--- array-manipulation-1/src/compact.js
+++ array-manipulation-1/src/compact.js
@@ -1,28 +1,12 @@
 /* exported compact */
 function compact(array) {
   var empty = [];
-  for (var i = 0; i < array.length; i++); {
-    if (typeof array[i] !== false) {
-      empty.push(array[i])
-    }
-    else if (array[i] === null) {
-      empty.push(array[i]);
-    }
-    else if (array[i] === NaN) {
-      empty.push(array[i]);
-    }
-    else if (array[i] === 0 && array[i] === -0) {
-      empty.push(array[i]);
-    }
-    else if (array[i] === undefined) {
-      empty.push(array[i]);
-    }
-    else if (typeof array[i] !== 'string') {
-      empty.push(array[i]);
-    }
-    else if (array[i] < empty) {
+  for (var i = 0; i < array.length; i++) {
+    if (array[i]) {
       empty.push(array[i]);
     }
   }
   return empty
 }
+
+/**N/A */
